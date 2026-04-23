1d64c7f9b978766de5c3966bf4ca2608c57175fd
velourcodes <mohammadaffansiddiqi98@gmail.com>
2026-04-23 11:36:22 +0530

    Updated subscription model for better record of usage

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,24 +2,34 @@ import mongoose from "mongoose";
 import { User } from "./user.model";
 
 const subscriptionSchema = mongoose.Schema({
-    subscription: {
-        isSubscribed: { type: Boolean, default: false },
+    isSubscribed: { type: Boolean, default: false },
 
-        plan: {
-            type: String,
-            enum: ["WEEKLY", "MONTHLY", "YEARLY", null],
-            default: null,
-        },
+    plan: {
+        type: String,
+        enum: ["WEEKLY", "MONTHLY", "YEARLY", null],
+        default: null,
+    },
 
-        startedAt: Date,
-        expiresOn: Date,
-        amount: Number,
-    },
-    usage: {
-        tryOnCount: { type: Number, default: 0, min: 0 },
-    },
+    startedAt: Date,
+    expiresOn: Date,
+    amount: Number,
     userId: {
         ref: User,
         type: mongoose.Schema.Types.ObjectId,
     },
+    usage: {
+        totalTryOns: {
+            type: Number,
+            min: 0,
+        },
+        dailyTryOns: {
+            type: Number,
+            min: 0,
+        },
+        dailyResetAt: {
+            type: Date,
+        },
+    },
 });
+
+export const Subscription = mongoose.Model("Subscription", subscriptionSchema);
